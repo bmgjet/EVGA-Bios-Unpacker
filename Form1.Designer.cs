df092afa7439f21e96d215871827fe32e8e273a0
bmg <bmgjet@gmail.com>
2020-12-11 14:23:49 +1300

    Version 0.2

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Romunpacker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 19);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(160, 225);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(178, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bios:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(178, 42);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Version:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(181, 181);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(223, 61);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Extract";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 3);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Address Range:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(181, 145);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(223, 31);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Open Update.exe";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 252);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "EVGA Bios Extractor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Romunpacker
+{
+    public partial class Form1 : Form
+    {
+//      EVGA Bios unpacker 0.2 by
+//     ▄▀▀█▄▄   ▄▀▀▄ ▄▀▄  ▄▀▀▀▀▄          ▄█  ▄▀▀█▄▄▄▄  ▄▀▀▀█▀▀▄ 
+//    ▐ ▄▀   █ █  █ ▀  █ █          ▄▀▀▀█▀ ▐ ▐  ▄▀   ▐ █    █  ▐ 
+//      █▄▄▄▀  ▐  █    █ █    ▀▄▄  █    █      █▄▄▄▄▄  ▐   █     
+//      █   █    █    █  █     █ █ ▐    █      █    ▌     █      
+//     ▄▀▄▄▄▀  ▄▀   ▄▀   ▐▀▄▄▄▄▀ ▐   ▄   ▀▄   ▄▀▄▄▄▄    ▄▀       
+//    █    ▐   █    █    ▐            ▀▀▀▀    █    ▐   █         
+//    ▐        ▐    ▐                         ▐        ▐ 
+
+        public byte[] file;
+        public string filename;
+        public int address;
+        public Form1()
+        {
+            InitializeComponent();
+            this.BringToFront();
+        }
+
+        string Search(byte[] src, byte[] pattern)
+        {
+            string foundaddress = "";
+            int c = src.Length - pattern.Length + 1;
+            int j;
+            for (int i = 0; i < c; i++)
+            {
+                for (j = pattern.Length - 1; j >= 1 && src[i + j] == pattern[j]; j--) ;
+                if (j == 0) foundaddress += i + ",";
+            }
+            return foundaddress;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        bool isDigits(string s)
+        {
+            if (s == null || s == "") return false;
+
+            for (int i = 0; i < s.Length; i++)
+                if ((s[i] ^ '0') > 9)
+                    return false;
+
+            return true;
+        }
+
+        public void startup()
+        {
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open EVGA Update";
+            theDialog.Filter = "Update.exe|Update.exe";
+            theDialog.InitialDirectory = @"C:\";
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                file = System.IO.File.ReadAllBytes(theDialog.FileName.ToString());
+                
+                //NVUFLASH_STRUCT
+                byte[] biostag = new byte[] {0x4E,0x56,0x55,0x46,0x4C,0x41,0x53,0x48,0x5f,0x53,0x54,0x52,0x55,0x43,0x54 };
+
+                string found = Search(file, biostag);
+                string[] result = found.Split(',');
+
+                foreach (string s in result)
+                {
+                    if (isDigits(s))
+                    {
+                            listBox1.Items.Add(s);
+                    }
+                }
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string text = listBox1.GetItemText(listBox1.SelectedItem);
+                address = int.Parse(text);
+
+
+                byte[] Biosname = new byte[19];
+                for (int i = 0; i < 19; i++)
+                {
+                    Biosname[i] = file[i + address + 29];
+                }
+
+                byte[] BiosVersion = new byte[14];
+                for (int i = 0; i < 14; i++)
+                {
+                    BiosVersion[i] = file[i + address + 112];
+                }
+
+                filename = Encoding.ASCII.GetString(Biosname);
+                label1.Text = "Bios: " + filename;
+                label2.Text = "Version: " + Encoding.ASCII.GetString(BiosVersion);
+            }
+            catch { }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.InitialDirectory = "C:\\";      
+            saveFileDialog1.Title = "Save Bios File";
+            saveFileDialog1.DefaultExt = "rom";
+            saveFileDialog1.FileName = filename;
+            saveFileDialog1.Filter = "rom files (*.rom)|*.rom";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                byte[] rom = new byte[999424];
+                for (int i = 0; i < 999424; i++)
+                {
+                    rom[i] = file[i + address + 144];
+                }
+                File.WriteAllBytes(saveFileDialog1.FileName, rom);
+                MessageBox.Show("File Saved");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            label2.Text = "Version: ";
+            label1.Text = "Bios: ";
+            startup();
+        }
+    }
+}
