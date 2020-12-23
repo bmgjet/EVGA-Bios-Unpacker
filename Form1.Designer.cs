5e7106e07bd5a461c8748ac7595c9021e3cf1569
bmg <bmgjet@gmail.com>
2020-12-23 16:59:19 +1300

    Fix crc for update.exe when patching your own bios into it.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,6 +38,7 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listBox1
@@ -47,7 +48,7 @@
             this.listBox1.Location = new System.Drawing.Point(12, 37);
             this.listBox1.Margin = new System.Windows.Forms.Padding(4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(316, 429);
+            this.listBox1.Size = new System.Drawing.Size(307, 429);
             this.listBox1.TabIndex = 0;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
@@ -64,7 +65,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(370, 83);
+            this.label2.Location = new System.Drawing.Point(369, 72);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(91, 25);
@@ -74,10 +75,10 @@
             // button1
             // 
             this.button1.Enabled = false;
-            this.button1.Location = new System.Drawing.Point(362, 348);
+            this.button1.Location = new System.Drawing.Point(332, 348);
             this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(446, 117);
+            this.button1.Size = new System.Drawing.Size(476, 117);
             this.button1.TabIndex = 3;
             this.button1.Text = "Extract";
             this.button1.UseVisualStyleBackColor = true;
@@ -95,10 +96,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(362, 279);
+            this.button2.Location = new System.Drawing.Point(332, 279);
             this.button2.Margin = new System.Windows.Forms.Padding(6);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(446, 60);
+            this.button2.Size = new System.Drawing.Size(476, 60);
             this.button2.TabIndex = 5;
             this.button2.Text = "Open Update.exe";
             this.button2.UseVisualStyleBackColor = true;
@@ -107,7 +108,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(370, 165);
+            this.label4.Location = new System.Drawing.Point(337, 107);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(123, 25);
@@ -117,7 +118,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(358, 208);
+            this.label5.Location = new System.Drawing.Point(327, 142);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(133, 25);
@@ -135,11 +136,22 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(397, 177);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 25);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "CRC:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(832, 485);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -155,7 +167,7 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "EVGA Bios Extractor 0.3";
+            this.Text = "EVGA Bios Extractor 0.4";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -173,6 +185,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,7 +7,7 @@ namespace Romunpacker
 {
     public partial class Form1 : Form
     {
-        //      EVGA Bios unpacker 0.3 by
+        //      EVGA Bios unpacker 0.4 by
         //     ▄▀▀█▄▄   ▄▀▀▄ ▄▀▄  ▄▀▀▀▀▄          ▄█  ▄▀▀█▄▄▄▄  ▄▀▀▀█▀▀▄ 
         //    ▐ ▄▀   █ █  █ ▀  █ █          ▄▀▀▀█▀ ▐ ▐  ▄▀   ▐ █    █  ▐ 
         //      █▄▄▄▀  ▐  █    █ █    ▀▄▄  █    █      █▄▄▄▄▄  ▐   █     
@@ -59,7 +59,7 @@ namespace Romunpacker
         {
             OpenFileDialog theDialog = new OpenFileDialog();
             theDialog.Title = "Open EVGA Update";
-            theDialog.Filter = "Update.exe|Update.exe";
+            theDialog.Filter = "Update.exe|*.exe";
             theDialog.InitialDirectory = @"C:\";
             if (theDialog.ShowDialog() == DialogResult.OK)
             {
@@ -122,7 +122,9 @@ namespace Romunpacker
                 PowerTable += 4443;
                 int Speed = BitConverter.ToInt32(BoostLimit, 0) / 1000;
                 int decWat = BitConverter.ToInt32(PowerLimit, 0) / 1000;
-                if (Speed > 2100 || Speed < 1395)
+
+                //Switch to type 2 if invalid data.
+                if (decWat > 1001 || decWat < 200)
                 {
                     for (int i = 0; i < 4; i++)
                     {
@@ -137,7 +139,69 @@ namespace Romunpacker
                     decWat = BitConverter.ToInt32(BoostLimit, 0) / 1000;
                 }
 
+                //Switch to type 3
+                if (decWat > 1001 || decWat < 200)
+                {
+                    PowerTable = 564097 + address + 0xCB4;
+                    PowerTable += 8969;
+                    for (int i = 0; i < 4; i++)
+                    {
+                        PowerLimit[i] = file[i + PowerTable];
+                    }
+                    PowerTable += 8969;
+                    for (int i = 0; i < 4; i++)
+                    {
+                        BoostLimit[i] = file[i + PowerTable];
+                    }
+                    PowerTable += 4443;
+                    Speed = BitConverter.ToInt32(BoostLimit, 0) / 1000;
+                    decWat = BitConverter.ToInt32(PowerLimit, 0) / 1000;
+                }
 
+                
+
+
+                byte[] crc = new byte[4];
+                for (int i = 0; i < 4; i++)
+                {
+                    crc[i] = file[i + address + 96];
+                }
+                var ReadCert = BitConverter.ToUInt32(crc,0);
+
+
+                byte[] rom = new byte[999424];
+                for (int i = 0; i < 999424; i++)
+                {
+                    rom[i] = file[i + address + 144];
+                }
+
+                var CalculatedCert = HULKCert_Algo(rom, 999424);
+
+                if (ReadCert == CalculatedCert)
+                {
+                    label6.ForeColor = System.Drawing.Color.Green;
+                }
+                else
+                {
+                    label6.ForeColor = System.Drawing.Color.Red;
+                }
+                label6.Text = "CRC: " + ReadCert.ToString("X2") + " [" + CalculatedCert.ToString("X2") + "]";
+                string cardtype = "";
+                ushort DeviceID = BitConverter.ToUInt16(rom, 37744 + 6);
+                if (DeviceID == 0x2204)
+                    cardtype = "RTX 3090";
+                else if (DeviceID == 0x2206)
+                    cardtype = "RTX 3080";
+
+                this.Text = "EVGA Bios Extractor 0.4 - "+ cardtype;
+
+                if (Speed == 0)
+                {
+                    if (DeviceID == 0x2204)
+                        Speed = 1695;
+                    else
+                        Speed = 1710;
+                }
 
                 filename = Encoding.ASCII.GetString(Biosname);
                 label1.Text = "Bios: " + filename;
@@ -162,6 +226,40 @@ namespace Romunpacker
                 button3.Enabled = false;
             }
         }
+
+
+
+        private ulong HULKCert_Algo(byte[] param_1, long param_2)
+
+        {
+            uint uVar1;
+            bool bVar2;
+            uint local_28;
+            uint local_24;
+            uint local_18;
+
+            local_24 = 0xffffffff;
+            local_28 = 0;
+            while (local_28 < param_2)
+            {
+                local_24 = local_24 ^ (uint)(param_1[local_28]) << 0x18;
+                local_18 = 0;
+                while (local_18 < 8)
+                {
+                    uVar1 = local_24 << 1;
+                    bVar2 = (int)local_24 < 0;
+                    local_24 = uVar1;
+                    if (bVar2)
+                    {
+                        local_24 = uVar1 ^ 0x4c11db7;
+                    }
+                    local_18 = local_18 + 1;
+                }
+                local_28 = local_28 + 1;
+            }
+            return (ulong)local_24;
+        }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -195,6 +293,7 @@ namespace Romunpacker
             startup();
         }
 
+
         internal static string StringFromHex(byte[] byte_0, int int_0, int int_1)
         {
             string @string = Encoding.ASCII.GetString(byte_0, int_0, int_1);
@@ -223,10 +322,20 @@ namespace Romunpacker
                     DialogResult dialogResult = MessageBox.Show("Are you sure you want to patch the exe with this .rom?" + Environment.NewLine + label2.Text + " to " + PBiosVersion, "Patch:", MessageBoxButtons.YesNo);
                     if (dialogResult == DialogResult.Yes)
                     {
+                        //write bios version string
                         for (int i = 0; i < 14; i++)
                         {
                             file[i + address + 112] = pfile[i + 37569];
                         }
+
+                        //Fix cert
+                        var CalculatedCert = HULKCert_Algo(pfile, 999424);
+                        byte[] HulkCert = BitConverter.GetBytes(CalculatedCert);
+                        for (int i = 0; i < 4; i++)
+                        {
+                            file[i + address + 96] = HulkCert[i];
+                        }
+
 
                         int patchedbyte = 0;
                         foreach (byte b in pfile)
